7e8fb21c3ffef50f3ad27a3003333f6ece6b3eb2
Daniel Lehmann <daniel.lehmann188@gmail.com>
2024-09-11 20:38:16 +0200

    added realtime updates

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { MatDialog } from '@angular/material/dialog';
 import { MatCardModule } from '@angular/material/card';
+import { Firestore, getDocs, collection } from '@angular/fire/firestore';
+import { CommonModule } from '@angular/common';
+import { onSnapshot, query, QuerySnapshot } from 'firebase/firestore';
 
 @Component({
   selector: 'app-user',
@@ -14,15 +17,28 @@ import { MatCardModule } from '@angular/material/card';
     MatButtonModule,
     MatTooltipModule,
     DialogAddUserComponent,
-    MatCardModule
+    MatCardModule,
+    CommonModule
   ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
 })
-export class UserComponent {
-  constructor(private dialog: MatDialog) {}
+export class UserComponent implements OnInit {
+  constructor(private dialog: MatDialog, private db: Firestore) {}
+
+  userFromDb: any;
+  usersAsArray: any[] = [];
 
   openDialog() {
     this.dialog.open(DialogAddUserComponent);
+  }
+
+  ngOnInit(): void {
+    const q = query(collection(this.db, 'users'));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        this.usersAsArray.push(doc.data());
+      })
+    })
   }
 }
