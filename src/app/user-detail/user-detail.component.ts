8bd5769509f33fc3791dbf21a4ad44abce5c480b
Daniel Lehmann <daniel.lehmann188@gmail.com>
2024-09-13 20:09:53 +0200

    added user detail header, todo: 2nd card with address info, get id from URL

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatCardModule
+  ],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.scss'
 })
